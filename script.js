9e15ef1b267a8a4ddf3b791636adbdfcd22279dc
burcu-erkan <burcuerkan.86@gmail.com>
2021-04-08 22:44:53 -0700

    added global obj

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // function renderdata
 
-let data;
+let result=[];
 const container = document.querySelector('.section-center');
 async function getData() {
     const response = await fetch('https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json');
     data = await response.json();
     return data;
 }
-
-getData().then(data => renderData(data));
-
+   getData().then(data => {
+    result=(data)
+    renderData(data)});
+  
 function renderData(data) {
   container.innerHTML='';
     for (item of data) {
@@ -27,3 +28,7 @@ function renderData(data) {
       </article>`
     }
 }
+
+
+
+
