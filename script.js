4078e978503cb5265f8ce53cbb380268954c2671
zhpadvoiski <zh.padvoiski@gmail.com>
2021-04-09 12:12:27 -0500

    onload event added

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 
 let result=[];
 const container = document.querySelector('.section-center');
+
+async function loadDataOnLoad(){
+  result = await getData();
+  renderData(result);
+  renderMenu();
+}
 async function getData() {
     const response = await fetch('https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json');
-    data = await response.json();
-    return data;
+    return await response.json();
 }
-   getData().then(data => {
-    result=(data)
-    renderData(data)});
-  
+
 function renderData(data) {
   container.innerHTML='';
     for (item of data) {
