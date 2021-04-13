13fbb44322f9a1c42f6749e46b2a96395bc7a145
carina <benazirihtiyorova22@gmail.com>
2021-04-12 21:14:24 -0700

    updated render data and menu

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// your code
+const menuDisplay = document.querySelector('.section-center')
+
+//fetching API
+window.addEventListener("DOMContentLoaded",() => {
+    fetch("https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json")
+    .then(response => response.json())
+    .then(data => renderData(data))
+})
+
+//rendering data
+function renderData(menuItems){
+    let menuData = menuItems.map(item => {
+        return `<article class="menu-item">
+        <img src=${item.img} class="photo" alt=${item.title}
+         />
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">$${item.price}</h4>
+          </header>
+          <p class="item-text">
+           ${item.desc}
+          </p>
+        </div>
+      </article>`
+    })
+    menuData = menuData.join('')
+    menuDisplay.innerHTML = menuData
+}
