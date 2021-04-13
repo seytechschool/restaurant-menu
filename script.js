22c76a6ab34d0beb3854d3b68ce35b731dbbe57e
ainura-t <stredeva@gmail.com>
2021-04-12 21:50:52 -0400

    added API getData

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code
+async function getData() {
+	const response = await fetch(
+		'https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json'
+	);
+	userData = await response.json();
+    
+    
+}
