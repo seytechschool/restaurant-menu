ce5bffc8cb002e285e11453bc530eccb13e102df
ainura-t <stredeva@gmail.com>
2021-04-12 22:50:35 -0400

    added renderData

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // your code
+
+
+const section = document.querySelector('.section-center');
+let userData;
 async function getData() {
 	const response = await fetch(
 		'https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json'
@@ -14,7 +18,7 @@ function renderMenu(arr) {
 	section.innerHTML = '';
 			for (let i = 0; i < arr.length; i++) {
 				section.innerHTML += `<article id="${arr[i].id}"class="menu-item" category="${arr[i].category}" >
-                        <img src='${arr[i].img}' class="photo" alt="${arr[i].title}" />
+                        <img src='${arr[i].img}' class="photo" alt="${arr[i].title}"/>
                         <div class="item-info">
                             <header>
                             <h4>${arr[i].title}</h4>
@@ -22,7 +26,7 @@ function renderMenu(arr) {
                             </header>
 							<p class="item-text">${arr[i].desc}</p>
 						</div>
-						<div class="a-cart"><button id="${arr[i].id}"class="cart">Add to cart</button></div>
+						<div class="a-cart"></div>
                         </article>`;
                         
             }
