13586f1f81261835e6700a373135ea33a8bfad8d
annpermi <perminova.anna85@gmail.com>
2021-04-13 23:00:40 -0400

    Added cart feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.section-center');
 const btnContainer = document.querySelector('.btn-container');
 const foundEl = document.querySelector('#found');
+const total = document.querySelector('.total');
 let data = []
 let filtered = [];
 /* Spinner */
@@ -8,14 +9,13 @@ foundEl.innerHTML = 'loading...'
 //fetched data from API
 async function fetchData() {
     const response = await fetch('https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json');
-    const data = await response.json();
-    //after calling data called rendering functions with our data
-    foundEl.innerHTML = '';
-    renderData(data)
-    renderButtons(data)
+     data = await response.json();
+        //after calling data called rendering functions with our data
+        foundEl.innerHTML = '';
+    renderData(data);
+    renderButtons(data);
 }
 fetchData()
-
     // rendered menu items
 function renderData(arr) {
     for (let item of arr) {
@@ -27,58 +27,61 @@ function renderData(arr) {
             <h4 class="price">${item.price}</h4>
             </header>
             <p class="item-text">${item.desc}</p>
+            <button class="add-cart" data-id="${item.id}">Add To Cart</button>
         </div>
         </article>`;
     }
+    // adding to the cart 
+    const cartBtns = document.querySelectorAll('.add-cart')
+    cartBtns.forEach(btn=>{
+        btn.addEventListener('click', addToCart)
+    })
 }
-
+let count = 0;
+function addToCart(e){
+    let id = e.target.dataset.id;
+    for(let item of data){
+        if(id===item.id.toString()){
+            count += item.price;    
+        }
+    }
+    total.innerHTML=count.toFixed(2);
+}
 // rendered category buttons
 function renderButtons(arr) {
-
-    //reduced data categories reduced to an array
-    const categories = arr.reduce(function(values, item) {
-        if (!values.includes(item.category)) {
-            values.push(item.category);
+    //creating categories buttons
+    const categories = arr.reduce(function(values, item){
+        if(!values.includes(item.category)){
+            values.push(item.category)
         }
-        return values;
-    }, ['all']);
-
-    //['all', 'breakfast', 'lunch', 'shakes', 'dinner']
-    //button array created
-    const categoryBtns = categories.map(function(category) {
-            return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
-    })
-    //button array converted to a string
-    .join('');
-    //btn string setted as an inner html                    
+        return values
+    },['all']);
+    const categoryBtns = categories.map(function(category){
+        return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
+    }).join('');
     btnContainer.innerHTML = categoryBtns;
-
-     //selecting and saving buttons
-     const buttons = btnContainer.querySelectorAll('.filter-btn');
-     buttons.forEach(button=>{
-         button.addEventListener('click',filterCategories)
-     })
-     let currActive = document.querySelector('.filter-btn');
-     buttons[0].click(); 
-     currActive.classList.add('active-btn');
-     
-     //add event listener to the category buttons
-     function filterCategories(e){
-         container.innerHTML='';
-         currActive.classList.remove('active-btn');
-         currActive = e.target;
-         let id = e.target.dataset.id;
-         e.target.classList.add('active-btn');
-         if(id==='all'){
-             filtered = arr;
-         } else {
-             filtered = arr.filter(item=>item.category===id);
-         }
-         renderData(filtered);
- }
-
+    //selecting and saving buttons
+    const buttons = btnContainer.querySelectorAll('.filter-btn');
+    buttons.forEach(button=>{
+        button.addEventListener('click',filterCategories)
+    })
+    let currActive = document.querySelector('.filter-btn');
+    buttons[0].click(); 
+    currActive.classList.add('active-btn');
+    function filterCategories(e){
+        container.innerHTML='';
+        currActive.classList.remove('active-btn');
+        currActive = e.target;
+        let id = e.target.dataset.id;
+        e.target.classList.add('active-btn');
+        if(id==='all'){
+            filtered = arr;
+        } else {
+            filtered = arr.filter(item=>item.category===id);
+        }
+        renderData(filtered);
 }
-
+};
 //A function for Input search
 function filterInput(arr, searchItem){
     let filteredChar = arr.filter(item => {
@@ -86,8 +89,6 @@ function filterInput(arr, searchItem){
     })
     renderData(filteredChar)
 }
- 
-
  //Input search
  const inputSearch = document.querySelector('#search');
  inputSearch.addEventListener('keyup', searching)
